3ea0c52376e8cab516a1b2105aed91d5bfc16b60
tiranoinpop <tiranoinpop@naver.com>
2025-12-02 09:14:55 +0900

    myclass

--- Rectangle/Rectangle.cpp
+++ Rectangle/Rectangle.cpp
@@ -2,6 +2,8 @@
 #include "Rectangle.h"
 using namespace std;
 
+//멤버 이니셜라이저
+//생성자로 생성자를 호출하여 하위 객체를 초기화
 Rectangle::Rectangle(const int &x1, const int &y1, const int &x2, const int &y2)
 :upLeft(x1,y1), lowRight(x2,y2)
 {
